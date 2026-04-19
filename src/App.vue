38690c7f95e32e85c910550a2d6191b1d4b8f76b
James Alejandro García Vásquez <scalapark68@gmail.com>
2026-04-19 11:25:27 -0500

    Revert "feat(operator): migrate operator page to Vue and align UI language"

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <router-view />
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
 </template>
 
 <style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import OperatorPage from '../views/OperatorPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'operator',
-      component: OperatorPage
-    }
-  ],
+  routes: [],
 })
 
 export default router
