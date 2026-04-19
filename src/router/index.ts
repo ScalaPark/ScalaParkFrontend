3e91c0bfe98ac56d83672d2e63370082722085be
MediGo Developer <dev@medigo.local>
2026-04-19 11:23:43 -0500

    feat(operator): migrate operator page to Vue and align UI language

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import OperatorPage from '../views/OperatorPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'operator',
+      component: OperatorPage
+    }
+  ],
 })
 
 export default router
